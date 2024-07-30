59bb98dfd61c831e1d0fa5f15f1a1be4a9a6b353
juliaret <julia.retuertanogales@alumnostrazos.net>
2024-07-30 21:37:56 +0200

    practica-casa-30-julio

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,35 @@
 
 import '../scss/styles.scss';
 
-button.addEventListener('click', () => {
-  console.log('BUTTON CLICKED');
-});
+// button.addEventListener('click', () => {
+//   console.log('BUTTON CLICKED');
+// });
+
+const tictactoeGameBoardElement = document.getElementById('tictactoe-game-board');
+
+const BOXES_PER_LINE = 3;
+
+const createGameBoard = () => {
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < BOXES_PER_LINE; i++) {
+    const newRow = document.createElement('div');
+    newRow.classList.add('tictactoe-game__row');
+
+    for (let j = 0; j < BOXES_PER_LINE; j++) {
+      const newBox = document.createElement('div');
+      newBox.classList.add('tictactoe-game__box');
+      newRow.append(newBox);
+    }
+
+    fragment.append(newRow);
+  }
+
+  tictactoeGameBoardElement.append(fragment);
+};
+
+const startGame = () => {
+  createGameBoard();
+};
+
+startGame();
